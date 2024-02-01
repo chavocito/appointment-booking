69a88f0341f3739c10d74ee99a13d221cc5dd869
chavocito <fiifiowusu@aol.com>
2024-02-01 15:02:59 +0000

    add sqlc files

---
+++ booking/db/query.sql
@@ -0,0 +1,15 @@
+-- name: InsertBooking :one
+INSERT INTO booking (start_time, end_time, email)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+
+-- name: ListBookingsBetween :many
+SELECT * FROM booking
+WHERE start_time >= $1 AND end_time <= $2;
+
+-- name: ListBookings :many
+SELECT * FROM booking;
+
+-- name: DeleteBooking :exec
+DELETE FROM booking WHERE id = $1;
